1d06b1baad2ac59323cc724d6411853573ba18fb
Roberto Luna <robertocgw@gmail.com>
2023-02-16 17:45:14 -0300

    fix: correção de lint

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,12 +1,3 @@
-"""Entenda a lógica da função de criptografia
-Recebe uma string message e um inteiro key como parâmetros
-Se key e message não possuírem os tipos corretos, uma exceção deve ser lançada
-Se key não for um índice positivo válido de message, retorna a string message invertida
-Se key for ímpar:
-divide message no índice key, inverte os caracteres de cada parte, e retorna a união das partes novamente com "_" entre elas
-Se key for par:
-divide message no índice key, inverte a posição das partes, inverte os caracteres de cada parte, e retorna a união das partes novamente com "_" entre elas"""
-
 import pytest
 from challenges.challenge_encrypt_message import encrypt_message
 
