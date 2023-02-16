6ff1ad923afee95199111cb1c87c941bb6a3f148
Roberto Luna <robertocgw@gmail.com>
2023-02-16 15:09:33 -0300

    Retirando comentários para tirar os erros de lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,9 +4,7 @@ def study_schedule(permanence_period, target_time):
 
     users_online = 0
 
-    # para cada intervalo de entrada e saida em permanence_period
     for init_time, end_time in permanence_period:
-        #se não tiver entrada ou saida, retorna none
         if not init_time or not end_time:
             return None
         if init_time <= target_time <= end_time:
