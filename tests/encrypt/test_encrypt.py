89c183931dda095c58f1e18c7870cedbc398f959
Roberto Luna <robertocgw@gmail.com>
2023-02-16 17:54:06 -0300

    feat: req2 - mais um caso de test

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,6 +3,8 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 4)
     with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message(1, '')
 
